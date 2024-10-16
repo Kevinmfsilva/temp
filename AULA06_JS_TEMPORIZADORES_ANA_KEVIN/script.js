0885d00505290579f8af64ba596d405fc6ece418
Kevinmfsilva <166409636+Kevinmfsilva@users.noreply.github.com>
2024-10-16 08:53:58 -0300

    Add files via upload

---
+++ AULA06_JS_TEMPORIZADORES_ANA_KEVIN/script.js
@@ -0,0 +1,31 @@
+// function acao(){
+//     document.write("executando....<br/>");
+// }
+
+
+
+//Temporizador
+//setInterval(acao,2000);
+
+
+// setInterval(()=>{
+//     document.write("executando atraves do arrow function...<br/>")
+// },1000);
+
+// clearInterval(timer);
+
+// var timer = setInterval (()=>{
+// document.write("executando atraves do arrow function...<br/>")
+// },1000);
+
+//outro temp
+function acao2(){
+    document.write("executando com o setTimeout!");
+
+}
+
+setTimeout(acao2,3000);
+
+setTimeout(()=>{
+    docukent.write("Executando com arrow function!")
+},4000);
